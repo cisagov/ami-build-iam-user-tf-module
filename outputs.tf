0732b7fb247733cf8d6b241032470e6cb463e043
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-22 00:39:18 -0400

    Define outputs

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,10 @@
-output "id" {
-  value       = aws_instance.example.id
-  description = "The EC2 instance ID"
+output "access_key" {
+  value       = aws_iam_access_key.key
+  description = "The IAM access key associated with the IAM user created by this module."
+  sensitive   = true
 }
 
-output "arn" {
-  value       = aws_instance.example.arn
-  description = "The EC2 instance ARN"
-}
-
-output "availability_zone" {
-  value       = aws_instance.example.availability_zone
-  description = "The AZ where the EC2 instance is deployed"
-}
-
-output "private_ip" {
-  value       = aws_instance.example.private_ip
-  description = "The private IP of the EC2 instance"
-}
-
-output "subnet_id" {
-  value       = aws_instance.example.subnet_id
-  description = "The ID of the subnet where the EC2 instance is deployed"
+output "user" {
+  value       = aws_iam_user.user
+  description = "The IAM user created by this module."
 }
