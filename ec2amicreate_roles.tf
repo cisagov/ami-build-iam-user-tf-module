f465225cea09f0baa9579728a31732e57a20b856
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-22 00:43:14 -0400

    Add customized EC2AMICreate roles/policies in Production and Staging Images accounts

---
+++ ec2amicreate_roles.tf
@@ -0,0 +1,69 @@
+# ------------------------------------------------------------------------------
+# Create the IAM role that allows creation of AMIs and read-only
+# access to any specified SSM Parameter Store parameters in the
+# Images accounts (Production and Staging).
+# ------------------------------------------------------------------------------
+
+# The Production role
+resource "aws_iam_role" "ec2amicreate_role_production" {
+  provider = aws.images-production-ami
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role_doc.json
+  description        = local.ec2amicreate_role_description
+  name               = local.ec2amicreate_role_name
+  tags = merge(var.tags,
+    {
+      "GitHub_Secret_Name"             = "BUILD_ROLE_TO_ASSUME",
+      "GitHub_Secret_Terraform_Lookup" = "arn"
+    }
+  )
+}
+
+# Attach the Production EC2AMICreate policy to the Production EC2AMICreate role
+resource "aws_iam_role_policy_attachment" "ec2amicreate_policy_attachment_production" {
+  provider = aws.images-production-ami
+
+  policy_arn = "arn:aws:iam::${local.images_production_account_id}:policy/${var.ec2amicreate_policy_name}"
+  role       = aws_iam_role.ec2amicreate_role_production.name
+}
+
+# Attach the Production ParameterStoreReadOnly policy
+# to the Production EC2AMICreate role
+resource "aws_iam_role_policy_attachment" "parameterstorereadonly_policy_attachment_production" {
+  provider = aws.images-production-ami
+
+  policy_arn = module.parameterstorereadonly_role_production.policy.arn
+  role       = aws_iam_role.ec2amicreate_role_production.name
+}
+
+# The Staging role
+resource "aws_iam_role" "ec2amicreate_role_staging" {
+  provider = aws.images-staging-ami
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role_doc.json
+  description        = local.ec2amicreate_role_description
+  name               = local.ec2amicreate_role_name
+  tags = merge(var.tags,
+    {
+      "GitHub_Secret_Name"             = "BUILD_ROLE_TO_ASSUME",
+      "GitHub_Secret_Terraform_Lookup" = "arn"
+    }
+  )
+}
+
+# Attach the Staging EC2AMICreate policy to the Staging EC2AMICreate role
+resource "aws_iam_role_policy_attachment" "ec2amicreate_policy_attachment_staging" {
+  provider = aws.images-staging-ami
+
+  policy_arn = "arn:aws:iam::${local.images_staging_account_id}:policy/${var.ec2amicreate_policy_name}"
+  role       = aws_iam_role.ec2amicreate_role_staging.name
+}
+
+# Attach the Staging ParameterStoreReadOnly policy
+# to the Staging EC2AMICreate role
+resource "aws_iam_role_policy_attachment" "parameterstorereadonly_policy_attachment_staging" {
+  provider = aws.images-staging-ami
+
+  policy_arn = module.parameterstorereadonly_role_staging.policy.arn
+  role       = aws_iam_role.ec2amicreate_role_staging.name
+}
