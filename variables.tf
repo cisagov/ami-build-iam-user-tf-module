121d948588bc21eb5f155be850dbf8e1c67c34db
Dave Redmin <david.redmin@beta.dhs.gov>
2020-04-02 15:17:36 -0400

    Explicitly define type for all string variables, since this was never done previously.

--- variables.tf
+++ variables.tf
@@ -10,6 +10,7 @@ variable "ssm_parameters" {
 }
 
 variable "user_name" {
+  type        = string
   description = "The name to associate with the AWS IAM user (e.g. test-ami-build-iam-user-tf-module)."
 }
 
@@ -20,16 +21,19 @@ variable "user_name" {
 # ------------------------------------------------------------------------------
 
 variable "ec2amicreate_policy_name" {
+  type        = string
   description = "The name of the IAM policy in the Images account that allows all of the actions needed to create an AMI."
   default     = "EC2AMICreate"
 }
 
 variable "ec2amicreate_role_description" {
+  type        = string
   description = "The description to associate with the IAM role that allows this IAM user to create AMIs.  Note that a \"%s\" in this value will get replaced with the user_name variable."
   default     = "Allows the %s IAM user to create AMIs."
 }
 
 variable "ec2amicreate_role_name" {
+  type        = string
   description = "The name to assign the IAM role that allows allows this IAM user to create AMIs.  Note that a \"%s\" in this value will get replaced with the user_name variable."
   default     = "EC2AMICreate-%s"
 }
