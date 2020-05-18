fce065f0bd8251c5e2e6057ca3898b1d0eb0113a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-18 14:41:46 -0400

    Fix TF formatting

--- user.tf
+++ user.tf
@@ -11,9 +11,9 @@ module "ci_user" {
     aws.staging    = aws.images-staging-ami
   }
 
-  role_description = local.ec2amicreate_role_description
+  role_description          = local.ec2amicreate_role_description
   role_max_session_duration = var.ec2amicreate_role_max_session_duration
-  role_name        = local.ec2amicreate_role_name
-  user_name        = var.user_name
-  tags             = var.tags
+  role_name                 = local.ec2amicreate_role_name
+  user_name                 = var.user_name
+  tags                      = var.tags
 }
