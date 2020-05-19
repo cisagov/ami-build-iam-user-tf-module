556b6f1ba7d3e12f98dc987a3c7f15e9147f3054
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-19 11:16:35 -0400

    Ensure that the prod and staging roles are tagged appropriately
    Otherwise the terraform-to-secrets script in cisagov/development-guide
    will not generate the appropriate secrets.

--- user.tf
+++ user.tf
@@ -11,9 +11,17 @@ module "ci_user" {
     aws.staging    = aws.images-staging-ami
   }
 
+  production_role_tags = {
+    "GitHub_Secret_Name"             = "BUILD_ROLE_TO_ASSUME_PRODUCTION",
+    "GitHub_Secret_Terraform_Lookup" = "arn",
+  }
   role_description          = local.ec2amicreate_role_description
   role_max_session_duration = var.ec2amicreate_role_max_session_duration
   role_name                 = local.ec2amicreate_role_name
-  user_name                 = var.user_name
-  tags                      = var.tags
+  staging_role_tags = {
+    "GitHub_Secret_Name"             = "BUILD_ROLE_TO_ASSUME_STAGING",
+    "GitHub_Secret_Terraform_Lookup" = "arn",
+  }
+  user_name = var.user_name
+  tags      = var.tags
 }
