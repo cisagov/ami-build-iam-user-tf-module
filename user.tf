880de23420912f33ac9b18ae06a444df8cdf3249
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-01-22 13:36:45 -0500

    Add a TODO comment
    TODO: Revert to using cisagov/ci-iam-user-tf-module after it has been updated to create single-environment IAM users.  For details, see: https://github.com/cisagov/ami-build-iam-user-tf-module/issues/63
    
    Co-authored-by: mcdonnnj <mcdonnnj@github.com>

--- user.tf
+++ user.tf
@@ -3,6 +3,9 @@
 # ------------------------------------------------------------------------------
 
 # The IAM user being created
+# TODO: Revert to using cisagov/ci-iam-user-tf-module after it has been updated
+# to create single-environment IAM users.  For details, see:
+# https://github.com/cisagov/ami-build-iam-user-tf-module/issues/63
 resource "aws_iam_user" "build" {
   provider = aws
 
